22c77c93626035384df860b07aeac2d58ffd593d
Ali Elawady <eali83051@gmail.com>
2024-03-21 03:44:34 +0200

    Adding The UType

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ let Answer = Req.body.Answer
 let Status = Req.body.Status
 let LoginTime = Req.body.LoginTime
 let LogoutTime = Req.body.LogoutTime
-let Type = Req.body.Type
 
 newUser._id = Code
 newUser.Name = Name
@@ -54,7 +53,7 @@ Res.json( [Data , Quesfind])
 })
 
 
-app.get("/User" , async(Req ,Res) =>{
+app.get("/UType" , async(Req ,Res) =>{
 Code = Req.body.Code
 let User = await Users.findById(Code)
 Res.json(User.Type)
